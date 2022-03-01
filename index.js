6a3d19dfe0c7ca84a3a3a01a4d3cf2caf03aac9b
thetravisweber <tbroweber@gmail.com>
2022-02-28 18:12:09 -0700

    server stores ownerships

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ let price = 100;
 
 wss.on('connection', (ws) => {
   const id = uuidv4();
-  const metadata = { id };
+  const ownerShips = [];
+  const score = 0;
+  const metadata = { id, ownerShips, score };
 
   clients.set(ws, metadata);
 
@@ -15,19 +17,34 @@ wss.on('connection', (ws) => {
 
   ws.on('message', (message) => {
     const metadata = clients.get(ws);
-
     const sender = metadata.id;
 
-    console.log(`${sender} is ${message}ing.`);
-
     if (message == "b") {
+      metadata.ownerShips.push(price);
       price+=10;
     } else if (message == "s") {
+      if (metadata.ownerShips.length == 0) {
+        return;
+      }
+      metadata.ownerShips = averageOwnerships(metadata.ownerShips);
+      metadata.score += price - metadata.ownerShips[0];
+      // remove 1 positon
+      metadata.ownerShips.splice(0, 1);
       price-=10;
     }
 
+    clients.set(ws, metadata);
+
+    returnData = {
+      "p": price,
+      "l": metadata.score
+    };
+
+    console.log(JSON.stringify(returnData));
     [...clients.keys()].forEach((client) => {
-      client.send(price);
+      client.send(
+        JSON.stringify(returnData)
+      );
     });
   });
 
@@ -37,6 +54,11 @@ wss.on('connection', (ws) => {
   });
 });
 
+function averageOwnerships(ownerShips) {
+  const average = [...ownerShips].reduce((a, b) => a + b) / ownerShips.length;
+  ownerShips.fill(average);
+  return ownerShips;
+}
 
 function uuidv4() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
@@ -44,4 +66,5 @@ function uuidv4() {
     return v.toString(16);
   });
 }
+
 console.log("wss up");
