b2780362f62ed80c238565bbfaf80fa83203e539
thetravisweber <tbroweber@gmail.com>
2022-03-06 10:49:53 -0700

    refactor: moved starting game loop to its own function

--- LineGame.js
+++ LineGame.js
@@ -6,11 +6,20 @@ class LineGame {
 
   constructor() {
     console.log("Line Game Created");
-    console.log(this.price);
+    this.startLoop();
+  }
+
+  startLoop() {
+    if (this.gameloopId != NO_GAME_LOOP) return;
     this.gameloopId = gl.setGameLoop(
       (delta) => {this.loop(delta)},
       1000 * 3
     );
+  }
+
+  endLoop() {
+    if (this.gameloopId == NO_GAME_LOOP) return;
+    gl.clearGameLoop(this.gameloopId);
   }
 
   // delta is time difference since last loop
