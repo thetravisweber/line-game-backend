9da41fd99dc8c5e0b615a98505b5ded47b09f1f9
thetravisweber <tbroweber@gmail.com>
2022-04-25 21:43:47 -0700

    removed some debug code

--- LineGame.js
+++ LineGame.js
@@ -157,10 +157,6 @@ class LineGame {
     leaderboard.sort(function(a, b) {
       return b.s - a.s;
     });
-    if (Math.random() < .1) 
-    {
-      console.log(leaderboard);
-    }
     return leaderboard;
   }
 
